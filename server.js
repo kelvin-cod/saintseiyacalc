15d5010ca44f3684b701db4f486895148edcdb99
Quebramedos <kelvin.zuzor@gmail.com>
2019-11-03 02:21:56 -0300

    update

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`$/dist/$`));
+app.use(express.static(__dirname + `/dist/saintSeiyaCalc`));
  
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`$/dist/$/index.html`));
+res.sendFile(path.join(__dirname +`$/dist/saintSeiyaCalc/index.html`));
 });
  
 app.listen(process.env.PORT || 8080);
